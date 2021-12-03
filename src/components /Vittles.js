861a2a67fc972fbd05c6073766c4989dc8dfbd6e
kyliepost <kylie.postlethwaite00@gmail.com>
2021-12-03 13:23:21 -0600

    setup

---
+++ src/components /Vittles.js
@@ -0,0 +1,11 @@
+import React from 'react' 
+import {ApplicationViews} from "./ApplicationVeiws.js" 
+import { NavBar } from "./nav/NavBar.js " 
+
+export const Vittles = () => { 
+    return ( <> 
+    <NavBar />
+    <ApplicationViews/> 
+    </>  
+    ) 
+}
